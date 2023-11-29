a32718b27c9910e7c917acaee9f3d1ff82022425
RianErick <rianzx34@gmail.com>
2023-11-29 17:36:05 -0300

    feat: security & auth2 :)

--- src/main/java/com/crud/rian/crud/security/SecurityConfig.java
+++ src/main/java/com/crud/rian/crud/security/SecurityConfig.java
@@ -3,21 +3,21 @@ package com.crud.rian.crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.cors(cors -> cors.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
-
-
-        
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(auth -> auth.jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).build();
     }
 }
