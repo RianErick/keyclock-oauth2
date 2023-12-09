2e78f6bf561bac1c384013469851cd9680d289e1
RianErick <rianzx34@gmail.com>
2023-12-09 18:34:13 -0300

    fix: pre auth of controller

--- src/main/java/com/crud/rian/crud/controller/LivroController.java
+++ src/main/java/com/crud/rian/crud/controller/LivroController.java
@@ -19,30 +19,32 @@ public class LivroController {
         this.livroService = livroService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public Livro adicionarLivro(@RequestBody Livro livro) {
         return livroService.adicionarLivro(livro);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')" )
     @GetMapping("/list-all")
     public List<Livro> listarLisvros() {
         return livroService.listarLivros();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public Livro buscarPorId(@PathVariable Long id) {
         return livroService.buscarPeloId(id);
     }
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public void deletarPorId(@PathVariable Long id) {
         livroService.deletarLivro(id);
     }
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/edit/{id}")
     public void atualizarLivro(@RequestBody Livro livro, @PathVariable Long id) {
         livroService.atualizarLivro(livro, id);
